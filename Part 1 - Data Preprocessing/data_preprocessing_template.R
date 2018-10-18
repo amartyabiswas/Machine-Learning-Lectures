192659004a57519f76c00e91340be9298d9296da
amartyabiswas <amartyabiswas001@gmail.com>
2018-10-18 08:22:51 +0530

    Data Preprocessing

---
+++ Part 1 - Data Preprocessing/data_preprocessing_template.R
@@ -0,0 +1,14 @@
+# Data preprocessing
+
+# Importing the dataset
+dataset = read.csv('Data.csv')
+
+# Taking care of the missing data
+dataset$Age=ifelse(is.na(dataset$Age),
+                   ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
+                   dataset$Age)
+
+# For salary
+dataset$Salary=ifelse(is.na(dataset$Salary),
+                   ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
+                   dataset$Salary)
